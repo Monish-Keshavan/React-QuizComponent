fcd0dc5331516a434dc227e524341ce52e9aef12
Monish-Keshavan <128992618+Monish-Keshavan@users.noreply.github.com>
2023-03-27 15:46:55 +0530

    13: Spelling Check

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,14 +16,14 @@ class Quiz extends React.Component{
             return {quiz_position: state.quiz_position+1}
         })
       }
-      handleRestClick(){
+      handleResetClick(){
         this.setState({quiz_position: 1})
       }
     render(){
         const isQuizEnd = ((this.state.quiz_position-1)===quizData.quiz_questions.length);
         return(
             <div>
-                {isQuizEnd? <QuizEnd resetClickHandler={this.handleRestClick.bind(this)}/> :
+                {isQuizEnd? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         )
